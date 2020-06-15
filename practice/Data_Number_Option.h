fb1821616776a1229e37d2d52488c8bf5ef8b5c9
MoonNight285 <65889750+MoonNight285@users.noreply.github.com>
2020-06-15 21:21:30 +0900

    Delete Data_Number_Option.h

--- practice/Data_Number_Option.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<list>
-#include<vector>
-using std::list;
-using std::vector;
-
-void data_New_Save(list<vector<char>>* data, vector<char>* temp, vector<int>* data_count);
-int data_Number_Count(list<vector<char>>* data, vector<char>* temp);
-void data_Number_Count_Plus(list<vector<char>>* data, vector<char>* temp, vector<int>* data_count, int* max);
